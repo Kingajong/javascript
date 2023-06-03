a0bb8ae88c203477c243896be60ad0f6609547d8
Kingajong <foletiasynclair63@gmail.com>
2023-06-03 22:23:20 +0100

    first commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/../assets/Chapter 1_Part 2-1.pdf'
+import CV from '../../assets/../assets/React Tuto (1) (1).pdf'
 const CTA = () => {
     return (
         <div className='cta'>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 import './CTA'
 import CTA from './CTA' //here we import the CTA component inside our header
-import me from '../../assets/naho1.jpg'
+import me from '../../assets/Synclair.jpg'
 import HeaderSocial from './HeaderSocial' //import of headerSocial component
 
 export const Header = () => {
@@ -10,7 +10,7 @@ export const Header = () => {
         <header>
             <div className='container header__container'>
                 <h5>Hello I'm</h5>
-                <h1>KOMBOU KOMGA MURIELLE NAHOMY </h1>
+                <h1>FOLETIA AJONGAKUE SYNCLAIR </h1>
                 <h5 className='text-light'>SOFTWARE ENGINEER </h5>
                 <CTA/>
                 <HeaderSocial/>
